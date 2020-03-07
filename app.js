dc91366e6a981a3c61208bb72fa62365c5c934ba
github <w0420789@nscc.ca>
2020-03-07 17:37:04 -0400

    buttons

--- app.js
+++ app.js
@@ -5,4 +5,14 @@ window.onload = () => {
     let text = document.createElement("H1");
     text.innerHTML = 'Hello';
     main.appendChild(text);
+
+    var goFS = document.getElementById("goFS");
+    goFS.addEventListener("click", function() {
+        main.requestFullscreen();
+    }, false);
+
+    let exitFS = document.getElementById("exitFS");
+    exitFS.addEventListener("click", function() {
+        document.exitFullscreen();
+    }, false);
 };
